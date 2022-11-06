17da04fcac3748a33b0e98123890548a8507823f
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-11-06 14:50:20 +0100

    Use out directory

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-arduino-cli compile $1 --fqbn Intel:arc32:arduino_101 ../WindLogger
+arduino-cli compile $1 --fqbn Intel:arc32:arduino_101 --build-path $PWD/out  ../WindLogger
--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-arduino-cli upload --fqbn Intel:arc32:arduino_101 --port /dev/ttyACM0
+arduino-cli upload --fqbn Intel:arc32:arduino_101 --port /dev/ttyACM0 --input-dir $PWD/out 
